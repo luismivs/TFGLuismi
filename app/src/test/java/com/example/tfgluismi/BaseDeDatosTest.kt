1935c2c6b28906b39012187a120b134d85cf8aba
LuisMiguelVS <luismivs.94@gmail.com>
2020-07-13 10:35:35 +0200

    Prueba locales

--- app/src/test/java/com/example/tfgluismi/BaseDeDatosTest.kt
+++ app/src/test/java/com/example/tfgluismi/BaseDeDatosTest.kt
@@ -6,19 +6,43 @@ import Data.Tarea
 import android.content.Context
 import org.junit.After
 import org.junit.Before
+import org.junit.runner.RunWith
 import java.io.IOException
 
 
 class BaseDeDatosTest {
-    private lateinit var tareas: ArrayList<String>
-    private lateinit var db: AdminBD
+    //private lateinit var tareas: ArrayList<String>
+    //private lateinit var db: AdminBD
 
-   /* @Before
-    fun createDb() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(
-            context, TestDatabase::class.java).build()
-        tareas = db.getTareas()!!
+   /* @RunWith(AndroidJUnit4::class)
+    class SimpleEntityReadWriteTest {
+        private lateinit var tareas: Tarea
+        private lateinit var db: AdminBD
+
+        @Before
+        fun createDb() {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            db = Room.inMemoryDatabaseBuilder(
+                context, TestDatabase::class.java).build()
+            userDao = db.getUserDao()
+        }
+
+        @After
+        @Throws(IOException::class)
+        fun closeDb() {
+            db.close()
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun writeUserAndReadInList() {
+            val user: User = TestUtil.createUser(3).apply {
+                setName("george")
+            }
+            userDao.insert(user)
+            val byName = userDao.findUsersByName("george")
+            assertThat(byName.get(0), equalTo(user))
+        }
     }*/
 
 }
--- app/src/test/java/com/example/tfgluismi/ExampleUnitTest.kt
+++ app/src/test/java/com/example/tfgluismi/ExampleUnitTest.kt
@@ -1,12 +1,57 @@
 package com.example.tfgluismi
 
+import Data.ArchCon
+import Data.ArchProy
+import Data.Proyecto
+import Data.Tarea
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class ExampleUnitTest {
+    private lateinit var tarea: Tarea
+    private lateinit var archCon: ArchCon
+    private lateinit var proy: Proyecto
+    private lateinit var archProy: ArchProy
+
+    @Before
+    fun createTarea() {
+       tarea = Tarea (0,"Tarea 1","","","","",0,"","")
+    }
+
+    @Before
+    fun createArchCon(){
+        archCon = ArchCon(0,"Archivo de consulta 1")
+    }
+
+    @Before
+    fun createProy(){
+        proy = Proyecto(0,"Proyecto 1", "")
+    }
+
+    @Before
+    fun createArchProy(){
+        archProy = ArchProy(0, "Archivo de proyecto 1", "Proyecto 1")
+    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun compare_tarea(){
+        assertEquals(tarea,Tarea (0,"Tarea 1","","","","",0,"",""))
+    }
+
+    @Test
+    fun compare_archCon(){
+        assertEquals(archCon,ArchCon(0,"Archivo de consulta 1"))
+    }
+
+    @Test
+    fun compare_proy(){
+        assertEquals(proy,Proyecto(0,"Proyecto 1", ""))
+    }
+
+    @Test
+    fun compare_ArchProy(){
+        assertEquals(archProy,ArchProy(0, "Archivo de proyecto 1", "Proyecto 1"))
     }
 }
